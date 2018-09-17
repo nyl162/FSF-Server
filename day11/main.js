6240667925956d439c624bdb2b238e13c1ae8dac
NGYL <nyl162@hotmail.com>
2018-09-17 12:31:30 +0800

    .

---
+++ day11/main.js
@@ -0,0 +1,34 @@
+// Load some libraries
+const express = require('express'); // look for [express directory] in [node_modules directory]. Other dependency refer to packege.json in package.
+const path = require('path');
+
+// create an instance of Express
+
+const app = express();
+
+// define our routes - rules to handle request.
+app.use(
+    express.static( // middleware to serve static files
+        path.join(__dirname, "public")
+    )
+);
+
+app.use(express.static(path.join(__dirname,"images")));
+
+
+//catch all
+app.use((req,resp)=>{
+    resp.status(404);
+    resp.sendFile(path.join(__dirname,'images','404logo.gif'))
+})
+
+
+// Start express and listen to a port.
+// app.listen(3000,function(){})
+app.listen(3000,() =>{
+    console.info('Application started on port 3000');
+    console.info('\truning directory is ', __dirname);
+    console.info('\tpublic directory is ', path.join(__dirname, "public"));
+
+})
+

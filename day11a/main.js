e024fcc897356164b91a4e171da715f035a9436f
NGYL <nyl162@hotmail.com>
2018-09-17 16:00:46 +0800

    .

--- day11a/main.js
+++ day11a/main.js
@@ -3,9 +3,18 @@ const path = require ("path");
 
 const app = express();
 
-app.use(express.static(path.join(__dirname,"dist","StarwarsApp")));
+//app.use(express.static(path.join(__dirname,"dist","StarwarsApp")));
 app.use(express.static(path.join(__dirname,"public")));
-app.use(express.static(path.join(__dirname,"media")));
+//app.use(express.static(path.join(__dirname,"media")));
+
+app.use((req,resp,next)=>{
+    //console.log("connection fail to -> ",req.url);
+    //resp.redirect("/404.html"); // good for do not let user know error can be use : resp.redirect("/");
+    
+    // good for API
+    resp.status(404); 
+    resp.sendfile(path.join(__dirname,"public","404.html"));
+})
 
 const PORT = parseInt(process.argv[2]) || parseInt(process.env.APP_PORT) || 3000
 

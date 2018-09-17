3d326b27c8d674315d32262fd652c0cf2262f97d
NGYL <nyl162@hotmail.com>
2018-09-17 15:23:04 +0800

    .

--- day11a/main.js
+++ day11a/main.js
@@ -7,6 +7,10 @@ app.use(express.static(path.join(__dirname,"dist","StarwarsApp")));
 app.use(express.static(path.join(__dirname,"public")));
 app.use(express.static(path.join(__dirname,"media")));
 
-app.listen(3000,()=>{
-    console.info("Application started on port 3000")
+const PORT = parseInt(process.argv[2]) || parseInt(process.env.APP_PORT) || 3000
+
+//console.log(process.env.APP_PORT);
+
+app.listen(PORT,()=>{
+    console.info(`Application started on port ${PORT} at ${new Date()}`)
 })

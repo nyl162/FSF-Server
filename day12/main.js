84496ea8f11242e8dccf9bcfd4aee3756c8b92ce
NGYL <nyl162@hotmail.com>
2018-09-18 13:05:15 +0800

    .

---
+++ day12/main.js
@@ -0,0 +1,15 @@
+const path = require ('path');
+const express = require ('express');
+
+const app = express();
+
+
+
+
+
+
+const PORT = parseInt(process.argv[2]) || parseInt(process.env.APP_PORT) || 3000
+
+app.listen ( PORT, ()=>{
+    console.info( `Application started at port ${PORT} at ${new Date()}`)
+})
